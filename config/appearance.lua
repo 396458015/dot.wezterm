3826c04e9defa2ce94bd506ccde76abc912e1d7c
396458015 <396458015@qq.com>
2023-11-25 17:06:22 +0800

    add chose dark or light theme and add without pic and pic.

--- config/appearance.lua
+++ config/appearance.lua
@@ -1,47 +1,44 @@
--- chose colorcheme
--- local theme = "catppucchin_latte" -- light
-local theme = "catppucchin_mocha" -- dark
+local Darkness = 1 -- 1: darkness, others: lightness
+local BG_pic = 0  -- 0: without pic, others: pic
 
+local theme = Darkness == 1 and "catppucchin_mocha" or "catppucchin_latte"
 local colors = require('colors.' .. theme)
-local light_theme_list= {
-    "catppucchin_latte",
-    "some colorcheme",
-}
-
 local wezterm = require('wezterm')
 
 local function setupTheme()
-    for _, value in ipairs(light_theme_list) do
-        if theme == value then
-            require('events.tab-title-light').setup()
-            require('events.right-status-light').setup()
-            return {
-                color_scheme = 'Catppuccin Latte',
-                opacity = 0.95,
-            }
-        end
-    end
-    require('events.tab-title-dark').setup()
-    require('events.right-status-dark').setup()
-    return {
-        color_scheme = 'Catppuccin Mocha',
+    local light_theme_list = {
+        "catppucchin_latte",
+        "some colorcheme",
+    }
+    local defaultTheme = {
+        color_scheme = 'Catppuccin Mocha', -- dark
         opacity = 0.97,
     }
+
+    local setupLightTheme = function()
+        require('events.tab-title-light').setup()
+        require('events.right-status-light').setup()
+        return {
+            color_scheme = 'Catppuccin Latte', -- light
+            opacity = 0.95,
+        }
+    end
+
+    for _, value in ipairs(light_theme_list) do
+        if theme == value then
+            return setupLightTheme()
+        end
+    end
+
+    require('events.tab-title-dark').setup()
+    require('events.right-status-dark').setup()
+    return defaultTheme
 end
 
--- random background
-local random_pic = {
-    -- '/backdrops/astro-jelly.jpg',
-    -- '/backdrops/final-showdown.jpg',
-    -- '/backdrops/pastel-samurai.jpg',
-    -- '/backdrops/punk.jpg',
-    '/backdrops/space.jpg',
-    '/backdrops/voyage.jpg',
-}
-local pic = math.random(1, #random_pic)
+local themeConfig = setupTheme()
 
-return {
-    color_scheme = setupTheme().color_scheme,
+local config = {
+    color_scheme = themeConfig.color_scheme,
     animation_fps = 60,
     max_fps = 60,
     front_end = 'WebGpu',
@@ -65,43 +62,55 @@ return {
         active_titlebar_bg = '#090909',
     },
     inactive_pane_hsb = { saturation = 1.0, brightness = 1.0 },
+}
 
-    background = {
+if BG_pic == 0 then
+    config.window_background_opacity = themeConfig.opacity
+else
+    local random_pic = {
+        '/backdrops/astro-jelly.jpg',
+        '/backdrops/final-showdown.jpg',
+        '/backdrops/pastel-samurai.jpg',
+        '/backdrops/punk.jpg',
+        '/backdrops/space.jpg',
+        '/backdrops/voyage.jpg',
+    }
+    local pic = math.random(1, #random_pic)
+
+    local backgroundImage = {
         {
-            -- img 
             -- source = { File = { path = wezterm.config_dir .. '/backdrops/smoke.gif', speed = 0.2} },
             -- source = { File = { path = "C:/Users/ThinkPad/.config/wezterm/backdrops/smoke.gif",speed = 0.2} },
             -- source = { File = { path = "C:/Users/ThinkPad/.config/wezterm/backdrops/man.gif",speed = 0.2} },
             -- source = { File = { path = "C:/Users/ThinkPad/.config/wezterm/backdrops/nasa.gif",speed = 0.2} },
-            -- source = { File = { path = wezterm.config_dir .. random_pic[pic] } },
-
-            -- pure color
-            source = { Color = 'black' },
+            source = { File = { path = wezterm.config_dir .. random_pic[pic] } },
         },
         {
             source = { Color = colors.background },
             height = '100%',
             width = '100%',
-            opacity = setupTheme().opacity,
+            opacity = themeConfig.opacity,
         },
-    },
+    }
+    config.background = BG_pic == 0 and { { source = { Color = colors.background }, opacity = themeConfig.opacity } } or backgroundImage
+end
 
-    wezterm.on("gui-startup", function(cmd)
-        local screen = wezterm.gui.screens().active
-        local tab, pane, window = wezterm.mux.spawn_window(cmd or {})
-        local gui = window:gui_window()
-        local width = 0.8 * screen.width
-        local height = 0.75 * screen.height
-        gui:set_inner_size(width, height)
-        gui:set_position((screen.width - width) / 2, (screen.height - height) / 4)
-    end),
-    wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
-        local pane = tab.active_pane
-        local title = pane.foreground_process_name
-        return {
-            { Text = " " .. title .. " " },
-        }
-    end),
-}
+wezterm.on("gui-startup", function(cmd)
+    local screen = wezterm.gui.screens().active
+    local tab, pane, window = wezterm.mux.spawn_window(cmd or {})
+    local gui = window:gui_window()
+    local width = 0.8 * screen.width
+    local height = 0.75 * screen.height
+    gui:set_inner_size(width, height)
+    gui:set_position((screen.width - width) / 2, (screen.height - height) / 4)
+end)
 
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+    local pane = tab.active_pane
+    local title = pane.foreground_process_name
+    return {
+        { Text = " " .. title .. " " },
+    }
+end)
 
+return config
